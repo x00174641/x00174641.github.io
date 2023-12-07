95ed691309fa9a1c4680c271e8835f22e5d583ac
Jason Fung <x00174641@mytudublin.ie>
2023-12-07 23:27:46 +0000

    Added grades.json and added js to read file and display to HTML

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+// Scrollspy
 let scrollSpy = new bootstrap.ScrollSpy(document.body, { target: '#navbarNavAltMarkup' })
 
+// Portfolio Cards Select as Active
 const cards = document.querySelectorAll('.card');
 let activeIndex = Math.floor(cards.length / 2);
 cards[activeIndex].classList.add('active-card');
@@ -29,3 +31,53 @@ document.getElementById('prevButton').addEventListener('click', () => {
         document.getElementById('prevButton').style.background = '#ccc'
     }
 });
+
+// Education Grades reading from JSON
+let jsonData = {};
+let currentYear = 'Year 3';
+
+function getGrades(year) {
+    currentYear = year; 
+    const tableBody = document.getElementById('gradesTable').getElementsByTagName('tbody')[0];
+    tableBody.innerHTML = '';
+
+    for ([course, grade] of Object.entries(jsonData[year])) {
+        const row = tableBody.insertRow();
+        const module = row.insertCell(0);
+        const result = row.insertCell(1);
+        module.textContent = course;
+        result.textContent = grade;
+    }
+    document.getElementById('currentYearResults').innerHTML = currentYear
+}
+
+window.onload = () => {
+    document.getElementById('nextYearBtn').style.background = '#ccc'
+    fetch('json/grades.json').then(response => response.json())
+    .then(data => {
+        jsonData = data;
+        getGrades('Year 3');
+    })
+};
+
+function changeYear(increment) {
+    const years = Object.keys(jsonData);
+    let index = years.indexOf(currentYear);
+    index += increment;
+
+    if (index >= 0 && index < years.length) {
+        getGrades(years[index]);
+    }
+
+    if (index == years.length-1) {
+        document.getElementById('nextYearBtn').style.background = '#ccc'
+    } else {
+        document.getElementById('nextYearBtn').style.background = '#fba056'
+    }
+
+    if (index == 0) {
+        document.getElementById('prevYearBtn').style.background = '#ccc'
+    } else {
+        document.getElementById('prevYearBtn').style.background = '#fba056'
+    }
+}
